94af608f23a3359083c409465b7731868864f48d
Diego04012002 <dgarciadeus@gmail.com>
2023-02-16 10:42:19 +0000

    Capital

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
 
 typewriter
   .pauseFor(2500)
-  .typeString('La Capital del Sol')
+  .typeString('La Capital de la playa')
   .pauseFor(200)
   .deleteChars(10)
   .start()
